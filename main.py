dbc15c4bb6c94a613e5180b590343c2988220af9
chlavo <ra.stu@seznam.cz>
2023-01-03 13:41:48 +0100

    ws

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import data
 from datetime import datetime
-
+import pandas as pd
 
 async def main():
     print(f"It's now {data.chromaticka_tabulka(0)}")
@@ -9,12 +9,19 @@ async def main():
     print('Hello, World! 123')
     print(f"It's now {datetime.now()}")
     async def foo():
+        df = pd.DataFrame({
+                "name":["karel", "ondrej"],
+                "vek":[45, 68]
+                })
         while True:
             output = datetime.now()
             pyscript.write("myNum", output)
             output1 = Element("myNum1")
             output1.write(datetime.now())
             await asyncio.sleep(1)
+            
+            output3 = Element("myPandas")
+            output3.write(df.head())
     await foo()
 
 #asaaas
